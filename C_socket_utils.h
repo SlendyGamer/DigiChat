9728056dc6c0d9ea9b6ed2dcf54bd55a1a943d49
Slendy_Gamer <gabrielcarotti@hotmail.com>
2025-08-27 21:10:48 -0300

    criado makefile para compilar projeto, library que contem algumas funcoes que serao usadas tanto no client como server para estabelecer novas conexoes e criado a main, que sera o client do projeto, e portanto sera renomeada depois para DigiChatClient.c

---
+++ C_socket_utils.h
@@ -0,0 +1,28 @@
+#ifndef C_SOCKET_UTILS_H_INCLUDED
+#define C_SOCKET_UTILS_H_INCLUDED
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+struct ClientSocket
+{
+    int conexaoSFD;
+    struct sockaddr_in conexaoAddr;
+    int erro;
+    bool respostaServer;
+};
+
+struct sockaddr_in* CriarEndereco_IPV4(char *ip, int port);
+
+int CriarSocketTCP_IPV4();
+
+struct ClienteSocket *AnalisarConexao(int serverSFD);
+
+
+#endif //C_SOCKET_UTILS_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,41 @@
+#include "C_socket_utils.h"
+#include <stdbool.h>
+
+int main()
+{
+    int SFD = CriarSocketTCP_IPV4(); //Cria um Socket File descriptor com dominio ipv4, tipo TCP e protocolo 0
+    
+    
+    struct sockaddr_in *addr = CriarEndereco_IPV4("127.0.0.1", 2000); //cria uma estrutura de endereco de rede usando dominio IPV4
+    int resposta = connect(SFD, (struct sockaddr*) addr, sizeof (*addr)); //tenta estabelecer conexao com servidor explicitado em addr
+
+    if (resposta == 0)//connect() retorna 0 se conexao for bem sucedida e -1 se falhou
+    {
+        printf("sucesso ao conectar!");
+    }
+    else
+    {
+        printf("fail");
+    }
+    
+    char *linhaDeInput = NULL;
+    size_t linhaDeInputTam = 0; //serao utilizados por getline para capturar o que usuario do client digitar
+    printf("Voce esta conectado, tente digitar algo!\n\n");
+
+    while (true)
+    {
+        ssize_t CaracteresTam = getline(&linhaDeInput, &linhaDeInputTam, stdin); //captura caracteres digitados até CR, incluindo ele
+        if(CaracteresTam>0)
+        {
+            if (strcmp(linhaDeInput, "/exit\n") == 0) //se digitado "/exit" e, em seguida Enter, encerra conexao
+            {
+                break;
+            }
+            ssize_t CaracteresEnviados = send(SFD, linhaDeInput, CaracteresTam, 0); //caso input seja diferente de exit, envia esse input ao server
+        }
+    }
+    close(SFD); //fecha o socket que esta aberto e encerra a conexao com o servidor
+}
+
+
+
